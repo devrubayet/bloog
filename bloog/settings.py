4ba14ec6291c7d6ed4f11d7c16225593644d5424
ersoft-128 <rubayethossain2003@gmail.com>
2024-05-20 02:50:10 +0600

    last update settings

--- bloog/settings.py
+++ bloog/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
    
 ]
-
+STATIC_ROOT =   'staticfiles/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
